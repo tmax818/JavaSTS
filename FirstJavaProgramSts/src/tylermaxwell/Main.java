dde1a6b74f797fb825c206724bff46f186593716
tmax818 <tylermaxwell661@gmail.com>
2022-12-09 12:03:52 -0800

    complete firstjavaprogram

--- FirstJavaProgramSts/src/tylermaxwell/Main.java
+++ FirstJavaProgramSts/src/tylermaxwell/Main.java
@@ -1,5 +1,10 @@
 package tylermaxwell;
 
 public class Main {
+	public static void main(String[] args) {
+		System.out.println("My name is Coding Dojo");
+		System.out.println("I am 100 years old");
+		System.out.println("My hometown is Burbank, CA");
+	}
 
 }
